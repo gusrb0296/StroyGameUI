bf2cca90c3b1e975025da4ac7057ec9bdb3b046e
GoHyunkyu <gusrb0296@naver.com>
2023-12-27 15:16:25 +0900

    [Feat] GameManager, UI Manager, Player, Folders
    게임메니저와 폴더를 생성했습니다.
    게임 매니저 싱글톤화와
    다른 기본 스크립트를 생성했습니다.

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if(!_instance)
+            {
+                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+
+                if( _instance == null ) 
+                {
+                    Debug.Log("No Singleton object");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (!_instance)
+        {
+            _instance = this;
+        }
+        else if( _instance != this )  // 인스턴스가 존재하는 경우 새로 생기는 인스턴스를 삭제한다.
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
